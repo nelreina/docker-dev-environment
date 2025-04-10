f255c7ab23ef8b6ef2e05b57b686f907a3eabdfb
Nelson Reina (@ibis-nreina) <nelson.reina@ibis-management.com>
2025-04-10 15:05:12 -0400

    prevent yno_routing_rules and system_registry to update github files

--- pb-hooks/utils.js
+++ pb-hooks/utils.js
@@ -3,40 +3,29 @@ const sendMessage = (channel, message) => {
   console.log("Sending message to clients", JSON.stringify(clients, null, 2))
 
   for (let clientId in clients) {
-      if (clients[clientId].hasSubscription(channel)) {
-          clients[clientId].send(message)
-      }
+    if (clients[clientId].hasSubscription(channel)) {
+      clients[clientId].send(message)
+    }
   }
 }
 
 module.exports = {
-    notify: (e, operation) => {
-      const { collectionName } = JSON.parse(JSON.stringify(e.record))
-      console.log(`${operation} record from collection name : ${collectionName}`)
-      if ("payments, payment_messages".includes(collectionName)) {
-      try {
-        
-        if (collectionName === "payments") {
-          sendMessage("payment_counter", {
-            name: "payment_counter",
-            data: JSON.stringify(e.record),
-          })
-        }
-      } catch (error) {
-       console.log(error )
-      }
-        return;
-      }
-      try {
-        const res = $http.send({
-          url: `http://ynohub-config-data:8000/re-cache/${collectionName}`,
-          method: "POST",
-          timeout: 120, // in seconds
-        })
-        const body = JSON.stringify(res.json)
-        console.log("Response from Collection data service re-cache", body)
-      } catch (error) {
-        console.log(error)
-      }
+  notify: (e, operation) => {
+    const { collectionName } = JSON.parse(JSON.stringify(e.record))
+    console.log(`${operation} record from collection name : ${collectionName}`)
+    if ("payments,payment_messages,system_registry,yno_routing_rules".includes(collectionName)) {
+      return;
     }
+    try {
+      const res = $http.send({
+        url: `http://ynohub-config-data:8000/re-cache/${collectionName}`,
+        method: "POST",
+        timeout: 120, // in seconds
+      })
+      const body = JSON.stringify(res.json)
+      console.log("Response from Collection data service re-cache", body)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 }
